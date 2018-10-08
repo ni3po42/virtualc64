544f5dcc135250fc274fcbb02fa55a53375056f9
Dirk Hoffmann <dirk.hoffmann@hs-karlsruhe.de>
2018-10-08 14:03:30 +0200

    Important bug fix in VC1541 constructor

--- C64/VC1541.cpp
+++ C64/VC1541.cpp
@@ -20,11 +20,11 @@
 
 #include "C64.h"
 
-VC1541::VC1541(unsigned deviceNr)
+VC1541::VC1541(unsigned nr)
 {
-    assert(deviceNr == 1 || deviceNr == 2);
+    assert(nr == 1 || nr == 2);
     
-    this->deviceNr = deviceNr;
+    deviceNr = nr;
     setDescription(deviceNr == 1 ? "Drive1" : "Drive2");
     debug(3, "Creating %s at address %p\n", getDescription());
 	
